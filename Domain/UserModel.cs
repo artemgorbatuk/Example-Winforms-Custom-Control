066ea4c4e29e5490cee6f925a103d2c5e5759fa2
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-04-11 13:07:04 +0300

    domain : создан проект и добавлена UserModel

---
+++ Domain/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+public class UserModel
+{
+    public string Name { get; set; } = default!;
+    public string Sex { get; set; } = default!;
+    public int Age { get; set; }
+    public bool Married { get; set; }
+}
