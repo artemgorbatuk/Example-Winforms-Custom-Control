cf5dbf7cdd717655ed3bf9ac3a74517b11ce5381
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-04-11 13:08:51 +0300

    content : реализован контрол UserCard

--- Content/FormMain.Designer.cs
+++ Content/FormMain.Designer.cs
@@ -28,17 +28,45 @@ partial class FormMain
     /// </summary>
     private void InitializeComponent()
     {
+        panelUserCards = new Panel();
+        userCard1 = new CustomControls.UserCard();
+        panelUserCards.SuspendLayout();
         SuspendLayout();
+        // 
+        // panelUserCards
+        // 
+        panelUserCards.Controls.Add(userCard1);
+        panelUserCards.Dock = DockStyle.Fill;
+        panelUserCards.Location = new Point(0, 0);
+        panelUserCards.Name = "panelUserCards";
+        panelUserCards.Size = new Size(882, 533);
+        panelUserCards.TabIndex = 0;
+        // 
+        // userCard1
+        // 
+        userCard1.BorderStyle = BorderStyle.FixedSingle;
+        userCard1.Location = new Point(3, 3);
+        userCard1.Name = "userCard1";
+        userCard1.Size = new Size(521, 191);
+        userCard1.TabIndex = 0;
         // 
         // FormMain
         // 
         AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(882, 533);
+        Controls.Add(panelUserCards);
+        MinimumSize = new Size(900, 580);
         Name = "FormMain";
+        StartPosition = FormStartPosition.CenterScreen;
         Text = "Main";
+        Load += FormMain_Load;
+        panelUserCards.ResumeLayout(false);
         ResumeLayout(false);
     }
 
     #endregion
+
+    private Panel panelUserCards;
+    private CustomControls.UserCard userCard1;
 }
--- Content/FormMain.cs
+++ Content/FormMain.cs
@@ -1,3 +1,6 @@
+using CustomControls;
+using Domain;
+
 namespace Content;
 
 public partial class FormMain : Form
@@ -5,5 +8,40 @@ public partial class FormMain : Form
     public FormMain()
     {
         InitializeComponent();
+    }
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+        var users = new[] {
+            new UserModel() { Name = "Name1", Sex = "Man", Age = 20, Married = true },
+            new UserModel() { Name = "Name2", Sex = "Woman", Age = 23, Married = true },
+            new UserModel() { Name = "Name3", Sex = "Man", Age = 32, Married = false },
+        }.ToList();
+        
+        panelUserCards.Controls.Clear();
+
+        foreach (var user in users) {
+
+            var userCard = new UserCard();
+
+            var index = users.IndexOf(user);
+
+            userCard.CreateUser(user, index);
+
+
+            userCard.CheckBoxMarriedChanged += (obj, args) =>
+            {
+                var married = (obj as UserCard)?.Married ?? false;
+
+                ShowChanges(married);
+            };
+
+            panelUserCards.Controls.Add(userCard);      
+        }
+    }
+
+    private void ShowChanges(bool married)
+    {
+        MessageBox.Show($"married: {married}");
     }
 }
